906c1ff5e28efd6b5952116367a2cd7c63b6b8a5
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-13 08:51:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    array[0]
+  when 2
+    "#{array[0]} and #{array[1]}"
+  else
+    last_str = ", and #{array.last}"
+    array[0...-1].join(", ") << last_str
+  end
 end
